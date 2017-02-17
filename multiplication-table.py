d80c9473d0058d6f57a4b93dfeab937afd4bf25a
samuelpych <samuelpych@hanovernorwichschools.org>
2017-02-17 12:52:58 -0500

    Updated from Brython Server: 2/17/2017 12:52:58 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Sam
+Credit: none
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,6 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+for i in ['x', 'x', 'x', 'x']:
+     for j in ['x','x']:
+         print(i,i,i,i,i,i,i,i,i,i)
